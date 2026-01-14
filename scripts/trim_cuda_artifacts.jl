934710ec5106950d9aac79549b86b10b916a9402
Anton Pozharskiy <apozharski@gmail.com>
2026-01-14 10:01:22 +0100

    script to get cuda artifact hashes

---
+++ scripts/trim_cuda_artifacts.jl
@@ -0,0 +1,19 @@
+using Pkg, Artifacts
+
+trimmed = ["CUDA_Runtime_jll", "CUDA_Driver_jll", "CUDA_Compiler_jll", "CUDSS_jll"]
+
+env = Pkg.Types.EnvCache(Base.active_project())
+
+pkgs = Pkg.Operations.load_all_deps(env)
+
+for pkg in pkgs
+    if pkg.name ∉ trimmed
+        continue
+    end
+    art = joinpath(Pkg.Operations.source_path(env.project_file, pkg), "Artifacts.toml")
+    parsed_art = Artifacts.parse_toml(art)
+    art_names = keys(parsed_art)
+    for art_name in art_names
+        println(Artifacts.artifact_hash(art_name, art))
+    end
+end
